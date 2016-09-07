6c6f557fbce2265d189cc85b4804b882f31b31d4
Jaap Schellekens <jaap@ws-fitzcarraldo.nl>
2016-09-07 13:35:10 +0200

    add sbm to project

--- src/dhydrology.cpp
+++ src/dhydrology.cpp
@@ -5,18 +5,19 @@
 
 #include "dhydrology.h"
 
-
-extern "C" {
-    EXPORT_API int initialize()
-    {
-        std::ostringstream msg;
-        return 0;
-    }
-}
-
 // placeholder function, all dll's need a main.. in windows only
 #if defined _WIN32
 void main()
 {
 }
 #endif
+
+extern "C" {
+    EXPORT_API int initialize()
+    {
+        return 0;
+    }
+
+
+}
+
